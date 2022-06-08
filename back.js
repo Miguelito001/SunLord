55ef7b27fa19dc3373b0072640be6e96739a7a56
Miguel <100603408+Miguelito001@users.noreply.github.com>
2022-06-08 16:32:49 -0300

    arrumadinha

--- back.js
+++ back.js
@@ -1,1 +1,43 @@
-consumo = 'consumo'
+var consumo = 1;
+var irradi = 1;
+var temp = 1;
+var consta = 1;
+var taxa = 1;
+var pot = 1;
+var tens = 1;
+var corrent = 1;
+var AC = 1;
+var max = 1;
+var min = 1;
+var correntMAX = 1;
+var MPPTs = 1;
+var qntINV = 1;
+var capCC = 1;
+var capCA = 1;
+var string = 1;
+var maxMODpSTR = 1;
+var numMOD = 1;
+function argumentos() {
+    var consumo = document.getElementById('consumo').value;
+    var irradi = document.getElementById('irradi').value;
+    var temp = document.getElementById('temp').value;
+    var consta = document.getElementById('consta').value;
+    var taxa = document.getElementById('taxa').value;
+    var pot = document.getElementById('temp').value;
+    var tens = document.getElementById('tens').value;
+    var corrent = document.getElementById('corrent').value;
+    var AC = document.getElementById('AC');
+    var max = document.getElementById('max');
+    var min = document.getElementById('min');
+    var correntMAX = document.getElementById('correntMAX');
+    var MPPTs = document.getElementById('MPPTs');
+    var qntINV = document.getElementById('qntINV');
+    var capCC = document.getElementById('capCC');
+    var capCA = document.getElementById('capCA');
+    var string = document.getElementById('string');
+    var maxMODpSTR = document.getElementById('maxMODpSTR');
+    var numMOD = document.getElementById('numMOD');
+    }
+function enviando(){
+    alert(consumo)
+}
